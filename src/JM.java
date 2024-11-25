15d094e64ba6b470e3f984687340ea917b869551
ErTitoBichu <90220229+ErTitoBichu@users.noreply.github.com>
2024-11-25 23:01:46 +0100

    "Modificación 5"

--- src/JM.java
+++ src/JM.java
@@ -5,6 +5,7 @@ public class JM {
 		System.out.println("Alumno: Javier Maroto");
 		System.out.println("Primera Modificación desde Master");
 		System.out.println("Modificación desde Dev");
+		System.out.println("Modificación desde GitHub");
 	}
 
 }
